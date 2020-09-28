6a4cb62933694a8dd7d290bbbfd55c09a7a3043e
Elisabeth Asbæk Jeppesen <6605388@RTS-365.dk>
2020-09-28 14:33:31 +0200

    add like a start? whoo flying start

---
+++ js/app.js
@@ -0,0 +1,29 @@
+let url = new URLSearchParams(window.location.search);
+
+let offset = url.get("offset") ? url.get("offset") : 0;
+let previousOffset, nextOffset;
+
+fetch(`https://swapi.dev/api/people?offset=${offset}`)
+    .then(res => res.json())
+    .then(function(data){
+        let maxOffset = data.count - (data.count % 15);
+
+        nextOffset = offset >= maxOffset ? maxOffset : parseInt(offset) + 15;
+        previousOffset = offset <= 0 ? 0 : parseInt(offset) - 15;
+
+        /* character template: */
+        let character = document.querySelector("#character");
+        /* character list container in main: */
+        let characterList = document.querySelector(".characterList");
+
+        data.results.forEach(function(result) {
+            let array = result.url.split("/");
+            let id = array[array.length -2];
+
+            let clone = character.content.cloneNode(true);
+            clone.querySelector(".character").innerText = result.name;
+            clone.querySelector(".character").href = `/character-sheet.html?id=${id}`;
+
+            characterList.appendChild(clone);
+        })
+    });
