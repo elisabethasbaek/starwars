d364c9444de295e78a2ada342591496bc0ec82d0
elisabeth asbæk jeppesen <elisabeth.asbaek@gmail.com>
2020-10-01 09:25:38 +0200

    add finish of planet details

--- js/planet-details.js
+++ js/planet-details.js
@@ -49,8 +49,8 @@ if (url.has("id")) {
                             let id = array[array.length - 2];
 
                             let clone = residentTemplate.content.cloneNode(true);
-                            clone.querySelector(".detailsPlanets__residents").innerText = data.name;
-                            clone.querySelector(".detailsPlanets__residents").href = `/people-sheet.html?id=${id}`;
+                            clone.querySelector(".details__resident").innerText = data.name;
+                            clone.querySelector(".details__resident").href = `/people-sheet.html?id=${id}`;
                             
                             residentList.appendChild(clone);
                         }) 
@@ -59,6 +59,25 @@ if (url.has("id")) {
                 document.querySelector(".category__residents").remove();
                 document.querySelector("#details__residents").remove();
             }
+
+            /* films: */
+            data.films.forEach(function(film){
+                fetch(film)
+                    .then(res => res.json())
+                    .then(function(data) { 
+                        let filmTemplate = document.querySelector("#details__films");
+                        let filmList = document.querySelector(".detailsPlanets__films");
+
+                        let array = data.url.split("/");
+                        let id = array[array.length - 2];
+
+                        let clone = filmTemplate.content.cloneNode(true);
+                        clone.querySelector(".details__film").innerText = data.title;
+                        clone.querySelector(".details__film").href = `/film-sheet.html?id=${id}`;
+                        
+                        filmList.appendChild(clone);
+                    }) 
+            })
 
         }) /* end of .then in fetch */
 
